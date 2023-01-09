efe1e7d850cc30a461b57715fc3ea51da7f80cd0
DhruvRawat888 <dhruv.rawat@geminisolutions.com>
2023-01-09 22:06:37 +0530

    Add project files.

---
+++ Assignment.View/ViewModel/HomepageViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Assignment.View.ViewModel
+{
+    public class HomepageViewModel
+    {
+
+        public string uname { get; set; }
+
+
+        private ICommand _logoutclick;
+        public ICommand logoutclick
+        {
+            get
+            {
+                return _logoutclick ?? (_logoutclick = new CommandHandler(() => logoutaction(), true));
+            }
+        }
+
+        public void logoutaction()
+        {
+
+            Homepage h = new Homepage();
+            h.Close();
+            MainWindow m = new MainWindow();
+            m.Show();
+
+        }
+
+
+
+
+
+        private ICommand _showtable;
+        public ICommand showtable
+        {
+            get
+            {
+                return _showtable ?? (_showtable = new CommandHandler(() => shtable(), true));
+            }
+        }
+
+        public void shtable()
+        {
+
+
+
+        }
+
+
+
+        public class CommandHandler : ICommand
+        {
+            private Action _action;
+            private bool _canExecute;
+            public CommandHandler(Action action, bool canExecute)
+            {
+                _action = action;
+                _canExecute = canExecute;
+            }
+
+            public bool CanExecute(object parameter)
+            {
+                return _canExecute;
+            }
+
+            public event EventHandler CanExecuteChanged;
+
+            public void Execute(object parameter)
+            {
+                _action();
+            }
+        }
+
+    }
+}
---
+++ Assignment.View/Views/Homepage.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace Assignment.View
+{
+    /// <summary>
+    /// Interaction logic for Homepage.xaml
+    /// </summary>
+    public partial class Homepage : Window
+    {
+        public Homepage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void logout(object sender, RoutedEventArgs e)
+        {
+            SqlConnection sqlcon = new SqlConnection(@"Data Source=(localdb)\Local;Initial Catalog=Assignments;Integrated Security=True;");
+            try
+            {
+                if (sqlcon.State == ConnectionState.Closed)
+                    sqlcon.Open();
+                string query = "select FirstName,LastName,Gender,Email,DOB from users";
+                SqlCommand sqlcmd = new SqlCommand(query, sqlcon);
+                sqlcmd.CommandType = CommandType.Text;
+                sqlcmd.ExecuteNonQuery();
+
+                SqlDataAdapter dataadp = new SqlDataAdapter(sqlcmd);
+                DataTable dt = new DataTable("users");
+                dataadp.Fill(dt);
+                datagrid.ItemsSource = dt.DefaultView;
+                dataadp.Update(dt);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlcon.Close();
+            }
+        }
+
+        private void searchclick(object sender, RoutedEventArgs e)
+        {
+            string sitem = searchbox.Text;
+
+            SqlConnection sqlcon = new SqlConnection(@"Data Source=(localdb)\Local;Initial Catalog=Assignments;Integrated Security=True;");
+            try
+            {
+                if (sqlcon.State == ConnectionState.Closed)
+                    sqlcon.Open();
+
+                string query = String.Format("select * from users where Firstname like '%{0}%' or LastName like '%{0}%'" +
+                    "  or Gender like '%{0}%'  or Email like '%{0}%'  or DOB like '%{0}%'", sitem);
+
+                //string query = "select * from Users where Firstname like '%@sitem%' or LastName like '%@sitem%'" +
+                // "  or Gender like '%@sitem%'  or Email like '%@sitem%'  or DOB like '%@sitem%' ";
+                SqlCommand sqlcmd = new SqlCommand(query, sqlcon);
+                sqlcmd.CommandType = CommandType.Text;
+
+                sqlcmd.ExecuteNonQuery();
+
+
+                SqlDataAdapter dataadp = new SqlDataAdapter(sqlcmd);
+                DataTable dt = new DataTable("users");
+                dataadp.Fill(dt);
+                datagrid.ItemsSource = dt.DefaultView;
+                dataadp.Update(dt);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlcon.Close();
+            }
+        }
+    }
+}
